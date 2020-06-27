3c85b93e257594c27cc4bf5ba89614e863adae03
Vladchere <vladchere.ok@mail.ru>
2020-06-27 17:34:19 +0700

    Initial Commit

---
+++ YoutubeClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeClient
+//
+//  Created by Vladislav on 27.06.2020.
+//  Copyright © 2020 Vladislav Cheremisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
